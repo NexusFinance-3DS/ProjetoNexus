c9530128343ee27c15dcbee91fe945ec08ccccfa
PauloHenriqueDev26 <paulo.benedicto3679@gmail.com>
2026-07-15 15:42:38 -0300

    Criação tela login e boas vindas

---
+++ NexusFinance/app/auth/boasVindas.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+import styles from "../styles/boasVindas";
+
+export default function boasVindas() {
+    const [erro, setErro] = useState("");
+
+    function criarConta() {
+        router.push("/auth/cadastro");
+    }
+
+    return (
+        <View style={styles.tela}>
+            <Image
+                source={require("../../assets/images/moedas.png")}
+                style={{
+                    alignSelf: "center",
+                    width: 150,
+                    height: 150,
+                    marginBottom: 60,
+                }}
+            />
+            
+            <Text style={styles.titulo}>Organize seus gastos de uma maneira mais eficiente!</Text>
+            <Text style={styles.subtitulo}>Controle seu orçamento e alcance suas metas financeiras com facilidade.</Text>
+
+            {erro ? <Text style={styles.erro}>{erro}</Text> : null}
+
+            <TouchableOpacity style={styles.botao} onPress={criarConta}>
+                <Text style={styles.textoBotao}>Criar Conta</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => router.push("/auth/login")}>
+                <Text style={styles.link}>Já tenho uma conta</Text>
+            </TouchableOpacity>
+            
+        </View>
+    );
+}
